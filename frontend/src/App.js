141f8c5ca903c35b3def1934c8f234ad1b82063a
Dimitar <dimiturivanov92@gmail.com>
2022-03-11 10:25:14 +0200

    husky test

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,6 +1,6 @@
 import styled from "styled-components";
-import { useState, useEffect } from 'react'
-import {BrowserRouter, Route, Routes } from 'react-router-dom'
+import { useState, useEffect } from "react";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
 import Home from "./pages/Home";
 import Header from "./components/Header";
 import Footer from "./components/Footer";
@@ -9,50 +9,42 @@ import GetAllProducstData from "./hooks-functions/GetAllProductsData";
 import Category from "./pages/Category";
 import Cart from "./pages/Cart";
 
-
-const MainContainer = styled.div`  
-`
+const MainContainer = styled.div``;
 function App() {
+	const [categories, setCategories] = useState([]);
 
-  const [categories, setCategories] = useState([])
+	const [products, setProducts] = useState([]);
 
-  const [products, setProducts] = useState([])
+	useEffect(() => {
+		getProductData();
+	}, []);
 
-  useEffect(() => {
-      getProductData()
-      
-  }, [])
+	const getProductData = async () => {
+		const Data = await GetAllProducstData("get-products");
 
-  const getProductData = async () => {
+		setCategories(Data.categories);
 
-    const Data = await GetAllProducstData('get-products')
+		setProducts(Data.products);
+	};
 
-    setCategories(Data.categories);
-
-    setProducts(Data.products)
-    
-  }
-
-  return (
-    <>
-      <MainContainer id='main-container'>
-          {categories.length !== 0 &&
-          <Header categories={categories} />
-        }
-        {products.length > 0 &&
-          <>
-              <Routes>
-                <Route path='/' element={<Home products={products} />} />
-                <Route path='/product/:id' element={<Product />} />
-                <Route path='/category/:name' element={<Category />} />
-                <Route path='/cart' element={<Cart />} />
-              </Routes>
-              <Footer />
-          </>
-        }
-      </MainContainer>
-    </>
-  );
+	return (
+		<>
+			<MainContainer id="main-container">
+				{categories.length !== 0 && <Header categories={categories} />}
+				{products.length > 0 && (
+					<>
+						<Routes>
+							<Route path="/" element={<Home products={products} />} />
+							<Route path="/product/:id" element={<Product />} />
+							<Route path="/category/:name" element={<Category />} />
+							<Route path="/cart" element={<Cart />} />
+						</Routes>
+						<Footer />
+					</>
+				)}
+			</MainContainer>
+		</>
+	);
 }
 
 export default App;
